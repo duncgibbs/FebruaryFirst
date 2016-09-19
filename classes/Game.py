fb3aba02e2dc0c632a00a36247c18c4da21c0ac6
Duncan Gibbs <duncgibbs@gmail.com>
2016-09-19 09:04:45 -0500

    generates 4*playerCount groundhogs at start

--- classes/Game.py
+++ classes/Game.py
@@ -1,8 +1,10 @@
 from classes.characters.Player import Player
+from classes.characters.Groundhog import Groundhog
 from classes.map.Map import Map
 
 class Game:
 	def __init__(self, playerCount):
 		self.playerList = [Player() for i in range(playerCount)]
+		self.groundhogList = [Groundhog() for i in range(4*playerCount)]
 		self.gameMap = Map(playerCount)
 
